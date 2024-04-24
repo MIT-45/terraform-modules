476d4ef290a7d121d99542bda20c14b5d2cc7ac6
Jared Spencer <jklz@me.com>
2024-04-24 12:52:24 -0600

    exclude dns entries from validation attempt

--- cloudflare-validation-for-aws-certificate/aws-acm-certificate-validation.tf
+++ cloudflare-validation-for-aws-certificate/aws-acm-certificate-validation.tf
@@ -1,8 +1,4 @@
 
 resource "aws_acm_certificate_validation" "validation" {
   certificate_arn         = var.certificate_arn
-  validation_record_fqdns = [
-    for record in cloudflare_record.validation :
-      record.hostname
-  ]
 }
