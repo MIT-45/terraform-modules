5c9c05af2743b884218582d3a51110511a16b23b
Jared Spencer <jklz@me.com>
2024-05-01 17:42:44 -0600

    removed deprecated quotes around type

--- elastic-beanstalk-nodejs/variables.tf
+++ elastic-beanstalk-nodejs/variables.tf
@@ -118,128 +118,128 @@ variable "port" {
 }
 
 variable "ssl_certificate_id" {
-  type    = "string"
+  type    = string
   default = ""
   description = "ARN of an SSL certificate to bind to the listener."
 }
 
 variable "healthcheck_url" {
-  type    = "string"
+  type    = string
   default = "HTTP:3000/healthcheck"
   description = "The path to which to send health check requests."
 }
 
 variable "healthcheck_healthy_threshold" {
-  type    = "string"
+  type    = string
   default = "3"
   description = "Consecutive successful requests before Elastic Load Balancing changes the instance health status."
 }
 
 variable "healthcheck_unhealthy_threshold" {
-  type    = "string"
+  type    = string
   default = "5"
   description = "Consecutive unsuccessful requests before Elastic Load Balancing changes the instance health status."
 }
 
 variable "healthcheck_interval" {
-  type    = "string"
+  type    = string
   default = "10"
   description = "The interval at which Elastic Load Balancing will check the health of your application's Amazon EC2 instances."
 }
 
 variable "healthcheck_timeout" {
-  type    = "string"
+  type    = string
   default = "5"
   description = "Number of seconds Elastic Load Balancing will wait for a response before it considers the instance nonresponsive."
 }
 
 variable "ignore_healthcheck" {
-  type    = "string"
+  type    = string
   default = "true"
   description = "Do not cancel a deployment due to failed health checks. (true | false)"
 }
 
 variable "healthreporting" {
-  type    = "string"
+  type    = string
   default = "basic"
   description = "Health reporting system (basic or enhanced). Enhanced health reporting requires a service role and a version 2 platform configuration."
 }
 
 variable "notification_topic_arn" {
-  type    = "string"
+  type    = string
   default = ""
   description = "Amazon Resource Name for the topic you subscribed to."
 }
 
 variable "enable_http" {
-  type = "string"
+  type = string
   default = "true"
   description = "Enable or disable default HTTP connection on port 80."
 }
 
 variable "enable_https" {
-  type = "string"
+  type = string
   default = "true"
   description = "Enable or disable HTTPS connection on port 443."
 }
 
 variable "elb_connection_timeout" {
-  type = "string"
+  type = string
   default = "60"
   description = "Number of seconds that the load balancer waits for any data to be sent or received over the connection."
 }
 
 # Auto Scaling
 variable "as_breach_duration" {
-  type = "string"
+  type = string
   default = "5"
   description = "Amount of time, in minutes, a metric can be beyond its defined limit (as specified in the UpperThreshold and LowerThreshold) before the trigger fires."
 }
 
 variable "as_lower_breach_scale_increment" {
-  type = "string"
+  type = string
   default = "-1"
   description = "How many Amazon EC2 instances to remove when performing a scaling activity."
 }
 
 variable "as_lower_threshold" {
-  type = "string"
+  type = string
   default = "2000000"
   description = "If the measurement falls below this number for the breach duration, a trigger is fired."
 }
 
 variable "as_measure_name" {
-  type = "string"
+  type = string
   default = "NetworkOut"
   description = "Metric used for your Auto Scaling trigger."
 }
 
 variable "as_period" {
-  type = "string"
+  type = string
   default = "5"
   description = "Specifies how frequently Amazon CloudWatch measures the metrics for your trigger."
 }
 
 variable "as_statistic" {
-  type = "string"
+  type = string
   default = "Average"
   description = "Statistic the trigger should use, such as Average."
 }
 
 variable "as_unit" {
-  type = "string"
+  type = string
   default = "Bytes"
   description = "Unit for the trigger measurement, such as Bytes."
 }
 
 variable "as_upper_breachs_scale_increment" {
-  type = "string"
+  type = string
   default = "1"
   description = "How many Amazon EC2 instances to add when performing a scaling activity."
 }
 
 variable "as_upper_threshold" {
-  type = "string"
+  type = string
   default = "6000000"
   description = "If the measurement is higher than this number for the breach duration, a trigger is fired."
 }
@@ -247,59 +247,59 @@ variable "as_upper_threshold" {
 # NodeJS
 # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-specific.html#command-options-nodejs
 variable "node_cmd" {
-  type    = "string"
+  type    = string
   default = ""
   description = "Command used to start the Node.js application."
 }
 
 variable "node_version" {
-  type    = "string"
+  type    = string
   default = "10.15.1"
   description = "Version of Node.js."
 }
 
 variable "proxy_server" {
-  type    = "string"
+  type    = string
   default = "none"
   description = "Specifies which web server should be used to proxy connections to Node.js."
 }
 
 variable "xray_enable" {
-  type    = "string"
+  type    = string
   default = "true"
 }
 
 # Security
 variable "vpc_id" {
-  type    = "string"
+  type    = string
   description = "The ID for your VPC."
 }
 
 variable "vpc_subnets" {
-  type    = "string"
+  type    = string
   description = "The IDs of the Auto Scaling group subnet or subnets."
 }
 
 variable "elb_subnets" {
-  type    = "string"
+  type    = string
   description = "The IDs of the subnet or subnets for the elastic load balancer."
 }
 
 variable "security_groups" {
-  type    = "string"
+  type    = string
   default = "elasticbeanstalk-default"
   description = "Lists the Amazon EC2 security groups to assign to the EC2 instances in the Auto Scaling group in order to define firewall rules for the instances."
 }
 
 # Elastic File Storage (Environment variables)
 variable "efs_id" {
-  type    = "string"
+  type    = string
   default = ""
   description = "The EFS ID to put in an EB Environment variable called EFS_ID."
 }
 
 variable "efs_mount_directory" {
-  type    = "string"
+  type    = string
   default = ""
   description = "The EFS Mount Directory to put in an EB Environment variable called EFS_MOUNT_DIRECTORY."
 }
